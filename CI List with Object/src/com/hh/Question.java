f7ccf4d446875ca3217b5a4c0eb07c5d98cebe5d
Steven_Huang <Steven_Huang@192.168.255.1>
2018-04-29 09:36:09 -0400

    CI with MAP. Iterate with MAP

--- CI List with Object/src/com/hh/Question.java
+++ CI List with Object/src/com/hh/Question.java
@@ -1,30 +1,32 @@
 package com.hh;
 
 import java.util.Iterator;
-import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class Question {
     private int id;
     private String question;
-    private List<Answer> answer;
+    private Map<String, String> answers;
     
     public Question(){}
     
-    public Question(int id, String question, List<Answer> answer) {
+    public Question(int id, String question, Map<String, String> answers) {
         super();
         this.id = id;
         this.question = question;
-        this.answer = answer;
+        this.answers = answers;
     }
     
     public void displayInfo() {
         System.out.println("id="+id);
         System.out.println("Question is:"+question);
-        System.out.println("Answer are:");
-        Iterator<Answer> it=answer.iterator();
+        System.out.println("Answers are:");
+        Set<Map.Entry<String, String>> set = answers.entrySet();
+        Iterator<Map.Entry<String, String>> it=set.iterator();
         while(it.hasNext()) {
-            System.out.println(it.next());
+        	Map.Entry<String, String> entry=it.next();
+            System.out.println("Answer："+entry.getKey()+" Posted by:"+entry.getValue());
         }
     }
 
